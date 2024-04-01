37d0f986bab8a94c11c52f1bc9be56121902f08b
lbejiuk <sergeyzarva@gmail.com>
2024-04-01 12:21:15 +0200

    some new services and update

---
+++ immich/update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+docker pull ghcr.io/immich-app/immich-web:release && \
+docker pull ghcr.io/immich-app/immich-server:release && \
+docker pull ghcr.io/immich-app/immich-server:release && \
+docker pull ghcr.io/immich-app/immich-machine-learning:release && \
+docker pull ghcr.io/immich-app/immich-proxy:release
